6aca921b32d8d19a79e9db968b96f3403917c072
Santanu26 <cushantanu@gmail.com>
2017-05-03 17:48:19 +0600

    6th Commit: ADD Registration Page

--- SpringBoot03/src/main/java/com/shantanu/controller/RegisterController.java
+++ SpringBoot03/src/main/java/com/shantanu/controller/RegisterController.java
@@ -1,17 +1,30 @@
 package com.shantanu.controller;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.shantanu.dto.RegistrationForm;
 
 //Declare @Controller annotation Spring will consider it when handling incoming request
 @Controller
 public class RegisterController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RegisterController.class);
+
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String registrationPage() {
 		LOGGER.debug("Rendering Registration Page...");
+		return "registration";
+	}
+
+	@RequestMapping(value = "/user/register", method = RequestMethod.POST)
+	public String userRegistration(@Valid RegistrationForm registrationForm , BindingResult result) {
+		System.out.println("User Page");
 		return "registration";
 	}
 }
