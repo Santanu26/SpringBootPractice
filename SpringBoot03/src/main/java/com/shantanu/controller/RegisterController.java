a77098a304aa0d8a63429ad224ae8e0a221c17e4
Santanu26 <cushantanu@gmail.com>
2017-05-03 17:12:13 +0600

    5th Commit: ADD L O G G E R

--- SpringBoot03/src/main/java/com/shantanu/controller/RegisterController.java
+++ SpringBoot03/src/main/java/com/shantanu/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.shantanu.controller;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 //Declare @Controller annotation Spring will consider it when handling incoming request
 @Controller
 public class RegisterController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RegisterController.class);
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String registrationPage() {
+		LOGGER.debug("Rendering Registration Page...");
 		return "registration";
 	}
 }
