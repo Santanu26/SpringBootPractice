ad2a93e9829898847196dd753aa13f759f308a7a
Santanu26 <cushantanu@gmail.com>
2017-05-03 14:04:38 +0600

    First Commit SpringBoot01 at 03 May 2017

---
+++ SpringBoot02/src/main/java/com/shantanu/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.shantanu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public String homeControllerPrintMessage() {
+		return "home";
+	}
+}
